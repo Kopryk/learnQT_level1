d7cdf2eebfc3b3b7ad0e945b5d54a65484e56b3c
Kamil Kopryk <koprykkamil@gmail.com>
2022-04-11 17:29:18 +0200

    Add qDebug example
    Signed-off-by: Kamil Kopryk <koprykkamil@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QCoreApplication>
 #include <QString>
 #include <iostream>
+#include <QDebug>
 
 
 void do_cpp(){
@@ -20,12 +21,20 @@ void do_qt(){
     textOut.flush();
 }
 
+void do_debug(){
+      QTextStream textIn(stdin);
+      qInfo() << "Please enter your name: ";
+      QString name = textIn.readLine();
+      qInfo() << name;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     //do_cpp();
-    do_qt();
+    //do_qt();
+    do_debug();
 
     return a.exec();
 }
